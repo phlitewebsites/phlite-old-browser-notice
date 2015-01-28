1971c850c5bc6fa922cad77f1aac702db8e4e5e7
Dan <Pythonic1@gmail.com>
2015-01-28 16:16:15 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = (function () {
+  var html = '' +
+    '<div id="oldBrowserNotice" class="wrap__oldBrowserNotice">' +
+      '<div class="oldBrowserNotice">' +
+        '<div class="inner__oldBrowserNotice">' +
+          '<div class="oldBrowserNotice__content">' +
+            '<div class="oldBrowserNotice__header">Upgrade Your Browser</div>' +
+            '<div class="oldBrowserNotice__body">' +
+              '<p>This website makes use of the latest web technologies to bring you the best online experience possible.' +
+                'We have detected that your current browser version is not the latest one.</p>' +
+              '<p>Please upgrade to avoid experiencing browsing issues. <b>Thank you!</b></p>' +
+              '<div class="oldBrowserNotice__links">' +
+                '<a target="blank" href="http://www.google.com/chrome">' +
+                  '<span class="oldBrowserNotice__image oldBrowserNotice__image--chrome"></span>' +
+                  '<span>Chrome</span>' +
+                '</a>' +
+                '<a target="blank" href="http://www.mozilla.org/">' +
+                  '<span class="oldBrowserNotice__image oldBrowserNotice__image--firefox"></span>' +
+                  '<span>Firefox</span>' +
+                '</a>' +
+                '<a target="blank" href="http://www.opera.com/">' +
+                  '<span class="oldBrowserNotice__image oldBrowserNotice__image--opera"></span>' +
+                  '<span>Opera</span>' +
+                '</a>' +
+                '<a target="blank" href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home">' +
+                  '<span class="oldBrowserNotice__image oldBrowserNotice__image--internet-explorer"></span>' +
+                  '<span>Internet Explorer</span>' +
+                '</a>' +
+              '</div>' +
+            '</div>' +
+            '<div onclick="document.body.removeChild(document.getElementById(\'oldBrowserNotice\'))"' +
+                'class="oldBrowserNotice__close" >&times</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+
+  document.write(html);
+})();
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Old Browser Notice
+
+```html
+<!doctype html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="oldBrowserNotice.css">
+    <script src="oldBrowserNotice.js"></script>
+  <![endif]-->
+</body>
+</html>
+```
+
+Intended for use with Browserify and Sass
